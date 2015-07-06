14c0f02dd8a5bbd1d50af1e48b014e8b60261dcb
papacastoro <rmarella88@gmail.com>
2015-07-06 13:09:32 +0200

    eureca :)

--- functions.php
+++ functions.php
@@ -4,12 +4,14 @@ function enqueue_parent_theme_style() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
-if ( function_exists('register_sidebar') ) {
-	register_sidebar(array(
-			'name'=>'sidebar2',
-			'before_widget' => '<li id="%1$s" class="widget %2$s">',
-			'after_widget' => '</li>',
-			'before_title' => '<h2 class="widgettitle">',
-			'after_title' => '</h2>',
-	));
-}
+	if (function_exists('register_sidebar')){
+		register_sidebar( array(
+			'name'          => ('Sidebar2'),
+			'id'            => 'sidebar2',
+			'description'   => '',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		));}
+	?>
--- sidebar2.php
+++ sidebar2.php
@@ -1,23 +1,3 @@
-<?php
-/**
- * The sidebar containing the secondary widget area
- *
- * Display on left screen
- *
- * If no active widgets are in this sidebar, hide it completely.
- *
- * @package WordPress
- */
-
-if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-	<div id="tertiary" class="sidebar-container" role="complementary">
-		<div class="sidebar-inner">
-			<div class="widget-area">
-				<?php dynamic_sidebar( 'sidebar-2' ); ?>
-			</div><!-- .widget-area -->
-		</div><!-- .sidebar-inner -->
-	</div><!-- #tertiary -->
-<?php endif; ?>
 
 <?php 
 if ( !function_exists('dynamic_sidebar') || ! dynamic_sidebar() ) :
